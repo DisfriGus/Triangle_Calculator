ab7f0c8ab79b7383030ef4844251a7742f4211b5
Dio Siregar <dio.siregar03@gmail.com>
2023-08-17 22:50:38 +0700

    feature

--- js/script.js
+++ js/script.js
@@ -5,27 +5,26 @@ let sisiA = document.getElementById('sisiA')
 let sisiB = document.getElementById('sisiB')
 let sisiC = document.getElementById('sisiC')
 const resultContainer = document.getElementsByClassName('resultArea')[0]
+const resultAround = document.getElementsByClassName('resultAround')[0]
 let resultParagraph = null;
-function handleSubmitArea (){
+let resultPAround = null;
+function handleSubmit (){
     let luas = (1/2) * alas.value * tinggi.value;
     if (resultParagraph) {
         resultParagraph.remove();
     }
-    const hasil = document.createElement('p');
-    hasil.textContent = `Luas Segitiga = ${luas}`;
-    resultContainer.appendChild(hasil);
-    resultParagraph = hasil;
-}
-const resultAround = document.getElementsByClassName('resultAround')[0]
-let resultPAround = null;
-function handleSubmitAround (){
+    const hasilLuas = document.createElement('p');
+    hasilLuas.textContent = `Luas Segitiga = ${luas}`;
+    resultContainer.appendChild(hasilLuas);
+    resultParagraph = hasilLuas;
     let keliling = Number(sisiA.value) + Number(sisiB.value) + Number(sisiC.value) ;
     if (resultPAround) {
         resultPAround.remove();
     }
-    const hasil = document.createElement('p');
-    hasil.textContent = `Keliling Segitiga = ${keliling}`;
-    resultAround.appendChild(hasil);
-    resultPAround = hasil;
+    const hasilKeliling = document.createElement('p');
+    hasilKeliling.textContent = `Keliling Segitiga = ${keliling}`;
+    resultAround.appendChild(hasilKeliling);
+    resultPAround = hasilKeliling;
 }
+
 
